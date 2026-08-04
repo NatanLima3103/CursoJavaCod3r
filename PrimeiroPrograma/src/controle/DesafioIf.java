54379c324c57da55030b43296c2aa12f39c53f5e
Natan Lima <natanjonatan3103@gmail.com>
2026-08-04 17:19:15 -0300

    DesafioIf

---
+++ PrimeiroPrograma/src/controle/DesafioIf.java
@@ -0,0 +1,21 @@
+package controle;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class DesafioIf {
+
+    public static void main (String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        System.out.println("Informe a nota: ");
+        double nota = entrada.nextDouble();
+
+        if (nota >= 9.0) {
+            System.out.println("Quadro de honra!");
+            System.out.println("Você é fera!!!");
+        }
+    }
+}
