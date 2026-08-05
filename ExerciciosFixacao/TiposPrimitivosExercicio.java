da24215dae2076d20d007e426ff0dd18ea490b17
Natan Lima <natanjonatan3103@gmail.com>
2026-08-05 14:45:02 -0300

    For3

---
+++ ExerciciosFixacao/TiposPrimitivosExercicio.java
@@ -0,0 +1,6 @@
+public class TiposPrimitivosExercicio {
+}
+// Crie variáveis para representar um produto de loja: nome (String),
+// preço (double), quantidade em estoque (int), se está em
+// promoção (boolean). Calcule e imprima o valor total em
+// estoque (preço × quantidade).
