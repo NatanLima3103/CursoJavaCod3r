b2c645e50eebd6c897309b794b5402463f5a98ba
Natan <natanjonatan3103@gmail.com>
2026-08-04 21:30:30 -0300

    WhileDeterminado

--- PrimeiroPrograma/src/controle/WhileDeterminado.java
+++ PrimeiroPrograma/src/controle/WhileDeterminado.java
@@ -4,7 +4,11 @@ public class WhileDeterminado {
 
     public static void main(String[] args) {
 
-        int
-        System.out.println("Bom dia!");
+        int contador = 1;
+
+        while(contador < 20 ) {
+            System.out.printf("i = %d\n", contador);
+            contador += 2;
+        }
     }
 }
