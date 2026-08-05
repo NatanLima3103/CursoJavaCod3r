b7394da794dc33f23c82b440ee392947e386ea83
Natan Lima <natanjonatan3103@gmail.com>
2026-08-05 19:51:26 -0300

    Resolução do exercício AritmeticosExercicio

--- ExerciciosFixacao/AritmeticosExercicio.java
+++ ExerciciosFixacao/AritmeticosExercicio.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class AritmeticosExercicio {
 
     //Peça três notas de um aluno. Calcule a média e o "resto" da
@@ -6,6 +8,23 @@ public class AritmeticosExercicio {
 
     public static void main (String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Informe a primeira nota: ");
+        double nota1 = entrada.nextDouble();
+
+        System.out.println("Informe a segunda nota: ");
+        double nota2 = entrada.nextDouble();
+
+        System.out.println("Informe a terceira nota: ");
+        double nota3 = entrada.nextDouble();
+
+        double media = ((nota1 + nota2 + nota3) / 3);
+
+        System.out.println("A média do aluno foi de: " + media);
+
+        double calcResto = (media % 2);
+
+        System.out.println("Resto da média: " + calcResto);
     }
 }
