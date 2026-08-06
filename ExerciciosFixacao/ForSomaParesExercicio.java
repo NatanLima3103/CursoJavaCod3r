71122fd6a5117c100008526ad0f66883e7293ca1
Natan Lima <natanjonatan3103@gmail.com>
2026-08-06 20:45:20 -0300

    Resolução exercício ForSomaParesExercicio

---
+++ ExerciciosFixacao/ForSomaParesExercicio.java
@@ -0,0 +1,28 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class ForSomaParesExercicio {
+
+    public static void main (String[] args) {
+
+        // 1. cria uma variável "soma", começando em 0, fora do for
+        // 2. o for percorre de 1 até 100
+        // 3. dentro do for, verifica: esse número é par?
+        // 4. se for, soma ele na variável "soma"
+        // 5. depois que o for terminar, imprime a soma
+
+        Scanner entrada = new Scanner(System.in);
+
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                soma = soma + i;
+            }
+        }
+
+        System.out.println("A soma dos números foi: " + soma);
+
+    }
+}
