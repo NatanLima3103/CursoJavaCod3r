14253ab3bee6b5ebe9ae3629c2081c7db10bd60e
Natan Lima <natanjonatan3103@gmail.com>
2026-08-05 21:21:13 -0300

    Resolução IfElseExercicio

--- ExerciciosFixacao/IfElseIfExercicio.java
+++ ExerciciosFixacao/IfElseIfExercicio.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class IfElseIfExercicio {
 
     //Peça a idade do usuário e classifique em faixa etária:
@@ -6,6 +8,21 @@ public class IfElseIfExercicio {
 
     public static void main (String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Informe sua idade: ");
+        int idade = entrada.nextInt();
+
+        if (idade <= 12) {
+            System.out.println("Você é uma crinça!");
+        } else if (idade > 12 && idade <= 17) {
+            System.out.println("Você é um adolescente!");
+        } else if (idade > 17 && idade <= 59) {
+            System.out.println("Você é um adulto!");
+        } else if (idade >= 60) {
+            System.out.println("você é um idoso!");
+        } else {
+            System.out.println("Informe uma idade válida!");
+        }
     }
 }
