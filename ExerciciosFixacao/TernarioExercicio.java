4fb7b0493e95752e4d7afcb6301fb66c5a1180b5
Natan Lima <natanjonatan3103@gmail.com>
2026-08-06 19:40:15 -0300

    Resolução do exercício TernarioExercicio

--- ExerciciosFixacao/TernarioExercicio.java
+++ ExerciciosFixacao/TernarioExercicio.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class TernarioExercicio {
 
     //Peça a idade do usuário. Usando ternário (sem if), imprima
     // "Maior de idade" ou "Menor de idade".
 
     public static void main (String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe sua idade:");
+        int idade = entrada.nextInt();
+
+        String eMaiorDeidade = idade >= 18 ? "Maior de idade" : "Menor de idade";
+
+        System.out.println("Você é " + eMaiorDeidade);
 
 
     }
