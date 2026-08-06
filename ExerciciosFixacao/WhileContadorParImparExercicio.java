2438f1b149664c76620ffd083627b9817798bbb4
Natan Lima <natanjonatan3103@gmail.com>
2026-08-06 15:38:56 -0300

    Resolução exercício WhileContadorParImparExercicio

---
+++ ExerciciosFixacao/WhileContadorParImparExercicio.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class WhileContadorParImparExercicio {
+
+    public static void main (String[] args) {
+
+        //Peça números ao usuário, um de cada vez, até ele digitar
+        // 0. Ao final, imprima quantos números pares e quantos números
+        // ímpares foram digitados.
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe um número:");
+        int num = entrada.nextInt();
+        int par = 0;
+        int impar = 0;
+
+        while (num != 0){
+
+            System.out.println("Informe outro número:");
+            num = entrada.nextInt();
+
+            if (num % 2 == 0) {
+                par = par + 1;
+
+            } else {
+                impar = num + 1;
+            }
+        }
+    }
+}
