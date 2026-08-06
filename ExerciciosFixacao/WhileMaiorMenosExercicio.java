4a872cfa5400e5c6ab6b3d82c930fa163386c8b5
Natan Lima <natanjonatan3103@gmail.com>
2026-08-06 15:17:35 -0300

    Resolução exercício WhileMaiorMenorExercicio

---
+++ ExerciciosFixacao/WhileMaiorMenosExercicio.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class WhileMaiorMenosExercicio {
+
+    public static void main (String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro número: ");
+        int num1 = entrada.nextInt();
+        int maior = num1;
+        int menor = num1;
+
+        while (num1 != 0) {
+            System.out.println("Informe outro número: ");
+            num1 = entrada.nextInt();
+
+            if (num1 > maior) {
+                maior = num1;
+            }
+
+            if (num1 < menor) {
+                menor = num1;
+            }
+        }
+
+        System.out.println("O maior valor é: " + maior);
+        System.out.println("O menor valor é: " + menor);
+
+
+        }
+    }
+
