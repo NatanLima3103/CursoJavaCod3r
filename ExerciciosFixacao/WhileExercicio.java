98d5d3ea0eace10d4bb9c6fee7d04fa3cb628937
Natan Lima <natanjonatan3103@gmail.com>
2026-08-06 10:31:56 -0300

    Resolução do exercício WhileExercicio

--- ExerciciosFixacao/WhileExercicio.java
+++ ExerciciosFixacao/WhileExercicio.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class WhileExercicio {
 
     //Some todos os números que o usuário digitar, um por vez,
@@ -6,6 +8,19 @@ public class WhileExercicio {
 
     public static void main (String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Escreva o número: ");
+        int num = entrada.nextInt();
+        int soma = 0;
+
+        while (num != 0) {
+            soma = soma + num;
+
+            System.out.println("Informe outro número: ");
+            num = entrada.nextInt();
+        }
+
+        System.out.println("A soma total foi: " + soma);
     }
 }
