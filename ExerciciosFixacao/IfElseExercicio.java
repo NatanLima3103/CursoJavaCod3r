d26b9285344c08a1544859e31176a5099b941b3f
Natan Lima <natanjonatan3103@gmail.com>
2026-08-05 21:11:56 -0300

    Resolução IfElseExercicio

--- ExerciciosFixacao/IfElseExercicio.java
+++ ExerciciosFixacao/IfElseExercicio.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class IfElseExercicio {
 
     //Peça um número. Se for divisível por 3 e por 5 ao mesmo
@@ -6,6 +8,18 @@ public class IfElseExercicio {
 
     public static void main (String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Informe o número: ");
+        int num = entrada.nextInt();
+
+        boolean divisivelPor3 = num % 3 == 0;
+        boolean divisivelPor5 = num % 5 == 0;
+
+        if (divisivelPor3 && divisivelPor5) {
+            System.out.println("FizzBuzz");
+        } else {
+            System.out.println(num);
+        }
     }
 }
