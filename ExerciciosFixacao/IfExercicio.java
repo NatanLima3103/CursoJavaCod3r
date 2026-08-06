378b96bceac8c9f34948e7df81d04c01dba9d64a
Natan Lima <natanjonatan3103@gmail.com>
2026-08-05 21:02:17 -0300

    Resolução exercício IfExercicio

--- ExerciciosFixacao/IfExercicio.java
+++ ExerciciosFixacao/IfExercicio.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class IfExercicio {
 
     //Peça um número. Se ele for maior que zero, imprima "Positivo!".
 
     public static void main (String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("informe o número: ");
+        int num = entrada.nextInt();
+
+        if (num > 0) {
+            System.out.println("Positivo!");
+        }
 
     }
 }
