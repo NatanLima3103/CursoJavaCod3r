9e3fbe93e6db6d52bad8fe0eb24c97d211958e83
Natan <natanjonatan3103@gmail.com>
2026-08-05 23:14:53 -0300

    Resolução exercício ForExercicio

--- ExerciciosFixacao/ForExercicio.java
+++ ExerciciosFixacao/ForExercicio.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ForExercicio {
 
     //Use um for para imprimir a tabuada de um número que
@@ -5,6 +7,13 @@ public class ForExercicio {
 
     public static void main (String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Informe um número para realizar a tabuada");
+        int num = entrada.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num * i);
+        }
     }
 }
