b3091a2b21b2babda7f0381cef01e2ee3c9707aa
Natan Lima <natanjonatan3103@gmail.com>
2026-08-07 14:00:29 -0300

    SwitchComBreak

---
+++ PrimeiroPrograma/src/controle/SwitchComBreak.java
@@ -0,0 +1,37 @@
+package controle;
+
+import java.util.Scanner;
+
+public class SwitchComBreak {
+
+    public static void main (String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String conceito = "";
+        System.out.println("Informa a nota: ");
+        int nota = entrada.nextInt();
+
+        switch(nota) {
+            case 10: case 9:
+                conceito = "A";
+                break;
+
+            case 8: case 7:
+                conceito = "B";
+                break;
+
+            case 6: case 5:
+                conceito = "C";
+                break;
+
+            case 4: case 3:
+                conceito = "D";
+                break;
+
+            default:
+                System.out.println("Conceito não informado!");
+
+        }
+        System.out.println("Conceito é: " + conceito);
+    }
+}
