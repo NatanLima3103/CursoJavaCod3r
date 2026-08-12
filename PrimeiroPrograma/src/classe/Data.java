49508d829c75cca36e198eebebd484cbb4d68cb9
Natan Lima <natanjonatan3103@gmail.com>
2026-08-12 10:17:51 -0300

    Resolução Desafio dos construtores

--- PrimeiroPrograma/src/classe/Data.java
+++ PrimeiroPrograma/src/classe/Data.java
@@ -6,11 +6,23 @@ public class Data {
     int mes;
     int ano;
 
+    Data () {
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+
+    Data (int diaInicial, int mesInicial, int anoInicial) {
+        dia = diaInicial;
+        mes = mesInicial;
+        ano = anoInicial;
+    }
+
     String obterDataFormatada() {
         return String.format("%d/%d/%d", dia, mes, ano);
     }
 
     void imprimirDataFormatada(){
-        System.out.printf(obterDataFormatada());
+        System.out.println(obterDataFormatada());
     }
 }
--- PrimeiroPrograma/src/classe/DataTeste.java
+++ PrimeiroPrograma/src/classe/DataTeste.java
@@ -5,20 +5,14 @@ public class DataTeste {
     public static void main (String[] args) {
 
         Data d1 = new Data();
-        d1.dia = 7;
-        d1.mes = 11;
         d1.ano = 2021;
 
-        Data d2 = new Data();
-        d2.dia = 31;
-        d2.mes = 12;
-        d2.ano = 2020;
+        var d2 = new Data(31, 12, 2020);
 
         String dataFormatada1 = d1.obterDataFormatada();
-        String dataFormatada2 = d2.obterDataFormatada();
 
         System.out.println(dataFormatada1);
-        System.out.println(dataFormatada2);
+        System.out.println(d2.obterDataFormatada());
 
         d1.imprimirDataFormatada();
         d2.imprimirDataFormatada();
