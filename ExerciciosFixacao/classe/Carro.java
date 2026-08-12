09952a7d4776369d513faad8b8537162d676673c
Natan <natanjonatan3103@gmail.com>
2026-08-11 21:14:29 -0300

    Resolução e criação do exercício CarroTeste e Carro

--- ExerciciosFixacao/classe/Carro.java
+++ ExerciciosFixacao/classe/Carro.java
@@ -6,19 +6,15 @@ public class Carro {
     double velocidadeAtual;
 
     public void acelerar(double incremento){
-        if (velocidadeAtual + incremento > velocidadeAtual) {
-            System.out.println("O carro acelerou");
-        } else {
-            System.out.println("O carro está em uma velocidade constante.");
-        }
+        velocidadeAtual = velocidadeAtual + incremento;
 
     }
 
     public void frear(double decremento) {
-        if (velocidadeAtual - decremento < velocidadeAtual) {
-            System.out.println("O carro está freando");
+        if (velocidadeAtual - decremento < 0) {
+            velocidadeAtual = 0;
         } else {
-            System.out.println("O carro está em uma velocidade constante.");
+            velocidadeAtual = velocidadeAtual - decremento;
         }
 
     }
--- ExerciciosFixacao/classe/CarroTeste.java
+++ ExerciciosFixacao/classe/CarroTeste.java
@@ -7,6 +7,12 @@ public class CarroTeste {
         Carro renault = new Carro();
         renault.modelo = "Symbol";
         renault.velocidadeAtual = 200;
+
+        renault.acelerar(50);
+        System.out.println("A velocidade após a aceleração ficou em: " + renault.velocidadeAtual);
+
+        renault.frear(250);
+        System.out.println("A velocidade após a freada ficou em: " + renault.velocidadeAtual);
     }
 
 }
