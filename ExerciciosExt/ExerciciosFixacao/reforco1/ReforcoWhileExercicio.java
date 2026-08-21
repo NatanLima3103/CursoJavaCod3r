6d7c74b05ba94c5b12b60042b2f155d431175e46
Natan Lima <natanjonatan3103@gmail.com>
2026-08-21 20:34:43 -0300

    ReforcoWhileExercicio

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/ReforcoWhileExercicio.java
@@ -0,0 +1,24 @@
+package reforco1;
+
+import java.util.Scanner;
+
+public class ReforcoWhileExercicio {
+
+    public static void main (String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe um número:");
+        int num = entrada.nextInt();
+        int calc = 0;
+
+        while (num >= 0){
+            calc = calc + num;
+            System.out.println("O valor total é: " + calc);
+
+            System.out.println("Informe outro número: ");
+            num = entrada.nextInt();
+
+        }
+    }
+}
