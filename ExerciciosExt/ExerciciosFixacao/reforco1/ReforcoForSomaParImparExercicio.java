ead6ca5c66897c98a6ae4063e997d7c1b96a24ba
Natan Lima <natanjonatan3103@gmail.com>
2026-08-21 19:50:40 -0300

    ReforcoForSomaParImparExercicio

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/ReforcoForSomaParImparExercicio.java
@@ -0,0 +1,28 @@
+package reforco1;
+
+import java.util.Scanner;
+
+public class ReforcoForSomaParImparExercicio {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        int num = entrada.nextInt();
+        int numPar = 0;
+        int numImpar = 0;
+
+        for (int i = 1; i <= num; i++){
+
+            if (i % 2 == 0){
+                numPar = i + numPar;
+            } else {
+                numImpar = i + numImpar;
+            }
+        }
+
+        System.out.println("A soma dos números pares é: " + numPar);
+        System.out.println("A soma dos números ímpares é: " + numImpar);
+    }
+}
