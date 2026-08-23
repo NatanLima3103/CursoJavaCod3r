a2e9a9a9870fbb0b76f3334417986798f3fc71c7
Natan Lima <natanjonatan3103@gmail.com>
2026-08-23 13:30:49 -0300

    Criação e resolução da classe e exercício Animal e AnimalTeste

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/Animal.java
@@ -0,0 +1,12 @@
+package reforco1;
+
+public class Animal {
+
+    String nome;
+    String especie;
+
+    Animal(String nome, String especie){
+        this.nome = nome;
+        this.especie = especie;
+    }
+}
