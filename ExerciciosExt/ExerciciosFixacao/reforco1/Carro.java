c618dfa79b1dec6e6ed22e039b9d3139e9388788
Natan Lima <natanjonatan3103@gmail.com>
2026-08-23 13:37:37 -0300

    Criação e resolução do exercício e classe Carro e CarroTeste

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/Carro.java
@@ -0,0 +1,12 @@
+package reforco1;
+
+public class Carro {
+
+    String modelo;
+    String proprietario;
+
+    Carro(String modelo, String proprietario){
+        this.modelo = modelo;
+        this.proprietario = proprietario;
+    }
+}
