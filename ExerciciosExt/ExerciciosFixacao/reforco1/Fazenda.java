ea9e52a6b70735b990c3af7ea54c729b2f18382a
Natan Lima <natanjonatan3103@gmail.com>
2026-08-24 16:52:10 -0300

    Criação e resolução do exercício e classe Fazenda e FazendaTeste

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/Fazenda.java
@@ -0,0 +1,13 @@
+package ExerciciosFixacao.reforco1;
+
+public class Fazenda {
+
+    String cidade;
+    String nome;
+
+    Fazenda (String cidade, String nome){
+
+        this.cidade = cidade;
+        this.nome = nome;
+    }
+}
