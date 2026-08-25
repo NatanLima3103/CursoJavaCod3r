f37508625e4503c5887ab7f04bafaee914f8737e
Natan Lima <natanjonatan3103@gmail.com>
2026-08-25 17:30:27 -0300

    ExercicioUm

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/array/ExercicioUm.java
@@ -0,0 +1,13 @@
+package ExerciciosFixacao.reforco1.array;
+
+public class ExercicioUm {
+
+    public static void main(String[] args) {
+
+        int [] numeros = {1, 5, 3, 6, 2, 7};
+
+        for(int i = 0; i < numeros.length; i++){
+            System.out.println(numeros[i] * 2);
+        }
+    }
+}
