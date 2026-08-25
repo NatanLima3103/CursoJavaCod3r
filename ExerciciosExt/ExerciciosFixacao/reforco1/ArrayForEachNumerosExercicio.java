2eb82d0add4a30a4c72911b5c5f42d3d47825f16
Natan <natanjonatan3103@gmail.com>
2026-08-24 21:48:24 -0300

    ArrayForEachMultiplosExercicio

--- ExerciciosExt/ExerciciosFixacao/reforco1/ArrayForEachNumerosExercicio.java
+++ ExerciciosExt/ExerciciosFixacao/reforco1/ArrayForEachNumerosExercicio.java
@@ -4,8 +4,6 @@ public class ArrayForEachNumerosExercicio {
 
     public static void main(String [] args) {
 
-        int numPar = 0;
-
         int [] numeros = {1, 2, 4, 5, 7, 8};
 
         for (int numero : numeros){
