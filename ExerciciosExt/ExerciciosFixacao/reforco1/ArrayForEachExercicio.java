a72b286d38fa00256e1772263a23c4f6f4d56be3
Natan <natanjonatan3103@gmail.com>
2026-08-24 21:07:07 -0300

    ArrayForEachExercicio

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/ArrayForEachExercicio.java
@@ -0,0 +1,13 @@
+package ExerciciosFixacao.reforco1;
+
+public class ArrayForEachExercicio {
+    public static void main(String [] args){
+
+        String[] frutas = {"Maçã", "Banana", "Uva", "Manga", "Pera"};
+
+        for(String fruta : frutas) {
+
+            System.out.println(fruta.toUpperCase());
+        }
+    }
+}
