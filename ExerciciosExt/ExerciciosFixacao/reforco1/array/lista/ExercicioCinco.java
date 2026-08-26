bd157de0d409e532515fa5f53f7edec5ff16cd73
Natan <natanjonatan3103@gmail.com>
2026-08-25 22:38:12 -0300

    ExercicioCinco

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/array/lista/ExercicioCinco.java
@@ -0,0 +1,14 @@
+package ExerciciosFixacao.reforco1.array.lista;
+
+public class ExercicioCinco {
+
+    public static void main(String[] args){
+
+        String [] nomes = {"Natan", "Ketney", "Jesiel", "Wellington", "Andrea"};
+
+        for (int i = 0; i < nomes.length; i++){
+
+            System.out.println(nomes[i] + " Posição: " +i);
+        }
+    }
+}
