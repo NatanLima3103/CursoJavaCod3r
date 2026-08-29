e709547c1f5c6666b1b41680c25e304b81bf232e
Natan Lima <natanjonatan3103@gmail.com>
2026-08-29 19:14:50 -0300

    Começo ExercicioSete

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/matriz/lista/ExercicioSete.java
@@ -0,0 +1,29 @@
+package reforco1.matriz.lista;
+
+import java.util.Scanner;
+
+public class ExercicioSete {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int [][] matriz = new int[3][3];
+
+        for(int linha = 0; linha < matriz.length; linha++){
+            for(int coluna = 0; linha < matriz[linha].length; coluna++){
+
+                System.out.println("Informe um valor: ");
+                int valorInf = entrada.nextInt();
+                matriz[linha][coluna] = valorInf;
+            }
+        }
+
+        for(int linha = 0; linha < matriz[0].length; linha++){
+            int valorPar = 0;
+
+            if(valorInf)
+            for(int coluna = 0; coluna < matriz.length; coluna++){
+
+            }
+        }
+    }
+}
