27eb1c57f96571470cbb0d2597bb95a1cb9c9a62
Natan <natanjonatan3103@gmail.com>
2026-09-01 23:07:46 -0300

    Exercicio valorVSReferencia Livro e LivroTeste

---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/valorVsReferencia/Livro.java
@@ -0,0 +1,13 @@
+package ExerciciosFixacao.reforco1.valorVsReferencia;
+
+public class ExercicioDois{
+
+    String titulo;
+    String autor;
+
+    ExercicioDois(String titulo, String autor){
+
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+}
---
+++ ExerciciosExt/ExerciciosFixacao/reforco1/valorVsReferencia/LivroTeste.java
@@ -0,0 +1,11 @@
+package ExerciciosFixacao.reforco1.valorVsReferencia;
+
+public class ExercicioDoisTeste {
+
+    public static void main(String[] args) {
+
+        ExercicioDois l1 = new ExercicioDois("Dom Casmurro", "Machado de Assis");
+
+        ExercicioDois l2 = l1;
+    }
+}
