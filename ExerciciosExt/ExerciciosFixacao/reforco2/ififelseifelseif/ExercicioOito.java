2e8aab01cdedef6caee2bab9207e59d9b97a8469
Natan Lima <natanjonatan3103@gmail.com>
2026-09-03 20:59:16 -0300

    Início ExercicioOito

---
+++ ExerciciosExt/ExerciciosFixacao/reforco2/ififelseifelseif/ExercicioOito.java
@@ -0,0 +1,29 @@
+package reforco2.ififelseifelseif;
+
+import java.util.Scanner;
+
+public class ExercicioOito {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe o valor da compra: ");
+        double valorCompra = entrada.nextInt();
+
+        System.out.println("Qual será a forma de pagamento?");
+        System.out.println("Selecione '1' para pagamento à vista (Pagamentos à vista possuem 10% de desconto, aproveite!).");
+        System.out.println("Selecione '2' para pagamento parcelado (Será acrescentado 5% ao valor da compra).");
+
+        int pagamento = entrada.nextInt();
+        double valorAVista = 0;
+        double valorCredito = 0;
+
+        if(pagamento == 1){
+            valorAVista = valorCompra * 0.9;
+            System.out.println("O valor da compra ficou em: " + valorAVista);
+        } else if(pagamento == 2){
+            valorCredito =
+
+        }
+    }
+}
