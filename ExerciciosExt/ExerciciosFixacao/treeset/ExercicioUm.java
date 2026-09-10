267f63a07712bb0bed716dc497477283a0639119
Natan <natanjonatan3103@gmail.com>
2026-09-09 21:03:39 -0300

    ExercicioUm

---
+++ ExerciciosExt/ExerciciosFixacao/treeset/ExercicioUm.java
@@ -0,0 +1,18 @@
+package treeset;
+
+import java.util.TreeSet;
+
+public class ExercicioUm {
+    public static void main(String[] args) {
+        TreeSet<String> nomes = new TreeSet<>();
+
+        nomes.add("Natan");
+        nomes.add("Lima");
+        nomes.add("Aline");
+        nomes.add("Fiori");
+        nomes.add("Wellington");
+        nomes.add("Ribeiro");
+
+        System.out.println(nomes);
+    }
+}
