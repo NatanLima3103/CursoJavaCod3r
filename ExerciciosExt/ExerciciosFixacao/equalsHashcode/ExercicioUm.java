30687231a2305bd153006e0052f0df4834c4870d
Natan <natanjonatan3103@gmail.com>
2026-09-15 22:25:19 -0300

    ExercicioDois

--- ExerciciosExt/ExerciciosFixacao/equalsHashcode/ExercicioUm.java
+++ ExerciciosExt/ExerciciosFixacao/equalsHashcode/ExercicioUm.java
@@ -1,5 +1,7 @@
 package equalsHashcode;
 
+import java.util.Objects;
+
 public class ExercicioUm {
     String nome;
     int codigo;
@@ -8,5 +10,17 @@ public class ExercicioUm {
 
         this.nome = nome;
         this.codigo = codigo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ExercicioUm that = (ExercicioUm) o;
+        return codigo == that.codigo && Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, codigo);
     }
 }
