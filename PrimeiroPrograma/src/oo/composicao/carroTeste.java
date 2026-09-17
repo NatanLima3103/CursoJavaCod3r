3383c5ddd971f1242d021913f32ed95ae2df9b02
Natan <natanjonatan3103@gmail.com>
2026-09-16 22:15:41 -0300

    Criação das classes Motor, Carro e CarroTeste

---
+++ PrimeiroPrograma/src/oo/composicao/carroTeste.java
@@ -0,0 +1,37 @@
+package oo.composicao;
+
+public class carroTeste {
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+        System.out.println("Está ligado? " + c1.estarLigado());
+
+        c1.ligar();
+        System.out.println("Está ligado? " + c1.estarLigado());
+
+        System.out.println(c1.motor.giros());
+
+        c1.acelerar();
+        c1.acelerar();
+        c1.acelerar();
+        c1.acelerar();
+
+        System.out.println(c1.motor.giros());
+
+        c1.frear();
+        c1.frear();
+        c1.frear();
+        c1.frear();
+        c1.frear();
+        c1.frear();
+        c1.frear();
+        c1.frear();
+
+        // Faltou Encapsulamento!!!
+        // c1.motor.fatorInjecao = -30;
+
+        System.out.println(c1.motor.giros());
+
+        // Relação bidimensional
+        System.out.println(c1.motor.carro.motor.carro.motor.giros());
+    }
+}
