ba64471244a7ddcb8a8cb85ffb3295b28350f2ed
Natan <natanjonatan3103@gmail.com>
2026-09-17 22:18:07 -0300

    Desafio Composição

---
+++ PrimeiroPrograma/src/oo/composicao/desafio/Item.java
@@ -0,0 +1,14 @@
+package oo.composicao.desafio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item {
+    final Produto produto;
+    final int quantidade;
+
+    Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+}
